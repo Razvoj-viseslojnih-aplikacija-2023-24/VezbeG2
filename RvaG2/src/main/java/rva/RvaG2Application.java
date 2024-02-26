fa29f9862ea55bee9bba79c17e37c491430a3979
vf98 <vf98@307-asistent.ftn.ns>
2024-02-26 12:52:25 +0100

    Prve vezbe - 26.02.2024.

---
+++ RvaG2/src/main/java/rva/RvaG2Application.java
@@ -0,0 +1,13 @@
+package rva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RvaG2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RvaG2Application.class, args);
+	}
+
+}
